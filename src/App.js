d93eb7059a45704fef842ee850bd6f9cbb10d5b0
Hisamitsu Okabe <okabe@worksap.co.jp>
2018-04-10 17:51:53 +0900

    feat: testing

--- src/App.js
+++ src/App.js
@@ -19,17 +19,55 @@ const schema = {
                     "child": {
                         type: "string", title: "I'm a child"}}}}}
     }
+
 };
 
 const log = (type) => console.log.bind(console, type);
 
+const data = {
+    "k1": "v1",
+    "k2": {
+        "k21": "v21"
+    }
+};
+
+let elem_final = [];
+let elem_parts = <div><span>Yes!</span></div>;
+let elem_form = <Form schema={schema}
+                        onChange={log("changed")}
+                        onSubmit={log("submitted")}
+                        onError={log("errors")}/>;
+
+elem_final.push(elem_parts);
+elem_final.push(elem_form);
+
 class App extends Component {
+    adjust_api_result() {
+        return <div id="api-root">Root!</div>;
+    };
+
+    createDom(data) {
+        if (!data) return null;
+
+        let contents = [];
+        for (let key of Object.keys(data)) {
+            let val = data[key];
+            contents.push(React.createElement('dt', null, key));
+            contents.push(React.createElement('dd', null, val && typeof val != "object" ? val : this.createDom(val)));
+        }
+        let dl = React.createElement('dl', null, contents);
+        return dl;
+    };
+
+    return_two() {
+        return 2;
+    }
     render() {
         return (
-            <Form schema={schema}
-                  onChange={log("changed")}
-                  onSubmit={log("submitted")}
-                  onError={log("errors")}/>
+            <div>
+                {this.adjust_api_result()}
+                {elem_final}
+            </div>
         );
     };
 }
--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,40 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('Return two', () => {
+  const app = new App();
+  const res = app.return_two();
+  expect(res).toEqual(2);
+});
+
+it('Adjust api response', () => {
+    const app = new App();
+    const res = app.adjust_api_result();
+    expect(res).toEqual(<div id="api-root">Root!</div>);
+});
+
+const data = {
+    "k1": "v1",
+    "k2": {
+        "k21": "v21"
+    }
+};
+const expected =
+    <dl>
+      <dt>k1</dt>
+        <dd>v1</dd>
+      <dt>k2</dt>
+        <dd>
+          <dl>
+              <dt>k21</dt>
+                <dd>v21</dd>
+          </dl>
+        </dd>
+    </dl>;
+
+it('Create DOM', () => {
+    const app = new App();
+    expect(app.createDom(data)).toEqual(expected);
+});
+
