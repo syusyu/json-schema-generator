929deb43c7cb7439d4995a13c60539d5744fb9e8
Hisamitsu Okabe <okabe@worksap.co.jp>
2018-04-11 15:34:20 +0900

    feat:  replace data

--- src/App.js
+++ src/App.js
@@ -3,8 +3,25 @@ import {render} from "react-dom";
 
 import Form from "react-jsonschema-form";
 
+const SCHEMA_GROUP = '_schemaGroup';
+
 const data_sample = {
-    k1: "v1"
+    "k1": "v1",
+    "k2": "v2",
+    "k3": {
+        "k31": "v31",
+        "k32": "v32",
+        "k33": [
+            {
+                "k331": "v331",
+                "k332": "v332",
+            },
+            {
+                "k333": "v333",
+                "k334": "v334",
+            }
+        ]
+    }
 };
 
 const schema_sample = {
@@ -15,13 +32,20 @@ const schema_sample = {
         title: {type: "string", title: "Title", default: "A new task"},
         done: {type: "boolean", title: "Done?", default: false},
         selection: {type: "integer", title: "Select!"},
-        grandpa: {type: "object", title: "", properties: {
-            "papa": {
-                type: "string", title: "I'm Papito"},
-            "mama": {
-                type: "object", title: "", properties: {
-                    "child": {
-                        type: "string", title: "I'm a child"}}}}}
+        grandpa: {
+            type: "object", title: "", properties: {
+                "papa": {
+                    type: "string", title: "I'm Papito"
+                },
+                "mama": {
+                    type: "object", title: "", properties: {
+                        "child": {
+                            type: "string", title: "I'm a child"
+                        }
+                    }
+                }
+            }
+        }
     }
 
 };
@@ -31,10 +55,8 @@ const log = (type) => console.log.bind(console, type);
 
 let elem_final = [];
 let elem_parts = <div><span>Yes!</span></div>;
-let elem_form = <Form schema={schema_sample}
-                        onChange={log("changed")}
-                        onSubmit={log("submitted")}
-                        onError={log("errors")}/>;
+let elem_form = <Form schema={schema_sample} onChange={log("changed")} onSubmit={log("submitted")}
+                      onError={log("errors")}/>;
 
 elem_final.push(elem_parts);
 elem_final.push(elem_form);
@@ -83,11 +105,44 @@ class App extends Component {
         return result;
     };
 
+    replaceDataBySchemaGroup(data, keyPrefix, keysForReplace, idxObj) {
+        if (Array.isArray(data)) {
+            let elements = [];
+            for (let e of data) {
+                elements.push(this.doReplaceDataBySchemaGroup(e, keyPrefix, keysForReplace, idxObj));
+            }
+            return elements;
+        } else {
+            return this.doReplaceDataBySchemaGroup(data, keyPrefix, keysForReplace, idxObj);
+        }
+    }
+
+    doReplaceDataBySchemaGroup(data, keyPrefix, keysForReplace, idxObj) {
+        let result = {};
+        let isPrevReplacable = false;
+        for (let key of Object.keys(data)) {
+            let wholeKey = keyPrefix ? keyPrefix + '.' + key: key;
+            let val = data[key];
+            if (keysForReplace && keysForReplace.includes(wholeKey)) {
+                let idx = isPrevReplacable ? idxObj.idx : ++idxObj.idx;
+                result[SCHEMA_GROUP + idx] = idx;
+                isPrevReplacable = true;
+            } else {
+                isPrevReplacable = false;
+                if (val && typeof val === 'object' || Array.isArray(val)) {
+                    result[key] = this.replaceDataBySchemaGroup(val, wholeKey, keysForReplace, idxObj);
+                } else {
+                    result[key] = val;
+                }
+            }
+        }
+        return result;
+    }
+
     render() {
         return (
             <div>
                 {this.createDom(data_sample)}
-                {elem_final}
             </div>
         );
     };
--- src/App.test.js
+++ src/App.test.js
@@ -6,37 +6,6 @@ const app = new App();
 
 /**************** createDom ***********************/
 const data = {
-    "k33": [
-        {
-            "k331": "v331",
-            "k332": "v332",
-        },
-        {
-            "k333": "v333",
-            "k334": "v334",
-        }
-    ]
-};
-const expected =
-    <dl>
-        <dt>k33</dt>
-        <dd>
-            <dl>
-                <dt>k331</dt>
-                <dd>v331</dd>
-                <dt>k332</dt>
-                <dd>v332</dd>
-            </dl>
-            <dl>
-                <dt>k333</dt>
-                <dd>v333</dd>
-                <dt>k334</dt>
-                <dd>v334</dd>
-            </dl>
-        </dd>
-    </dl>;
-
-    const data2 = {
     "k1": "v1",
     "k2": "v2",
     "k3": {
@@ -54,7 +23,7 @@ const expected =
         ]
     }
 };
-const expected2 =
+const expected =
     <dl>
         <dt>k1</dt>
         <dd>v1</dd>
@@ -87,7 +56,7 @@ const expected2 =
     </dl>;
 
 it('Create DOM', () => {
-    expect(app.createDom(data2)).toEqual(expected2);
+    expect(app.createDom(data)).toEqual(expected);
 });
 
 /**************** schemaReplaceKeys ***********************/
@@ -99,13 +68,20 @@ const schema = {
         title: {type: "string", title: "Title", default: "A new task"},
         done: {type: "boolean", title: "Done?", default: false},
         selection: {type: "integer", title: "Select!"},
-        grandpa: {type: "object", title: "", properties: {
+        grandpa: {
+            type: "object", title: "", properties: {
                 "papa": {
-                    type: "string", title: "I'm Papito"},
+                    type: "string", title: "I'm Papito"
+                },
                 "mama": {
                     type: "object", title: "", properties: {
                         "child": {
-                            type: "string", title: "I'm a child"}}}}}
+                            type: "string", title: "I'm a child"
+                        }
+                    }
+                }
+            }
+        }
     }
 };
 const expectedKeys = [`title`, `done`, `selection`, `grandpa.papa`, `grandpa.mama.child`];
@@ -113,3 +89,99 @@ const expectedKeys = [`title`, `done`, `selection`, `grandpa.papa`, `grandpa.mam
 it('Get schema replace keys', () => {
     expect(app.schemaReplaceKeys(schema)).toEqual(expectedKeys);
 });
+
+/**************** schemaGroup ***********************/
+const replaceKeys = ['k1', 'k3.k31', 'k3.k33.k331', 'k3.k33.k333'];
+const replaceKeys2 = ['k2', 'k3.k31', 'k3.k32', 'k3.k33.k331'];
+const replaceKeys3 = ['k3.k33.k331', 'k3.k33.k332', 'k3.k33.k333', 'k3.k33.k334'];
+const replaceKeys4 = [];
+const replaceKeys5 = null;
+
+const expectedReplaceData = {
+    "_schemaGroup1": 1,
+    "k2": "v2",
+    "k3": {
+        "_schemaGroup2": 2,
+        "k32": "v32",
+        "k33": [
+            {
+                "_schemaGroup3": 3,
+                "k332": "v332",
+            },
+            {
+                "_schemaGroup4": 4,
+                "k334": "v334",
+            }
+        ]
+    }
+};
+const expectedReplaceData2 = {
+    "k1": "v1",
+    "_schemaGroup1": 1,
+    "k3": {
+        "_schemaGroup2": 2,
+        "k33": [
+            {
+                "_schemaGroup3": 3,
+                "k332": "v332",
+            },
+            {
+                "k333": "v333",
+                "k334": "v334",
+            }
+        ]
+    }
+};
+
+const expectedReplaceData3 = {
+    "k1": "v1",
+    "k2": "v2",
+    "k3": {
+        "k31": "v31",
+        "k32": "v32",
+        "k33": [
+            {
+                "_schemaGroup1": 1
+            },
+            {
+                "_schemaGroup2": 2
+            }
+        ]
+    }
+};
+
+const expectedNonReplaceData = {
+    "k1": "v1",
+    "k2": "v2",
+    "k3": {
+        "k31": "v31",
+        "k32": "v32",
+        "k33": [
+            {
+                "k331": "v331",
+                "k332": "v332",
+            },
+            {
+                "k333": "v333",
+                "k334": "v334",
+            }
+        ]
+    }
+};
+
+let idxObj = {idx: 0};
+it('Replace data by keys', () => {
+    idxObj.idx = 0;
+    expect(app.replaceDataBySchemaGroup(data, '', replaceKeys, idxObj)).toEqual(expectedReplaceData);
+    idxObj.idx = 0;
+    expect(app.replaceDataBySchemaGroup(data, '', replaceKeys2, idxObj)).toEqual(expectedReplaceData2);
+    idxObj.idx = 0;
+    expect(app.replaceDataBySchemaGroup(data, '', replaceKeys3, idxObj)).toEqual(expectedReplaceData3);
+    idxObj.idx = 0;
+    expect(app.replaceDataBySchemaGroup(data, '', replaceKeys4, idxObj)).toEqual(expectedNonReplaceData);
+    idxObj.idx = 0;
+    expect(app.replaceDataBySchemaGroup(data, '', replaceKeys5, idxObj)).toEqual(expectedNonReplaceData);
+});
+
+
+
